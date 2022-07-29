5a5f957021f27d37bf315ce5f363fe48f21c957a
Adriane Caroline <adrianecaroline23@gmail.com>
2022-07-28 22:10:31 -0300

    commit-alteracoes

--- ouvidoria/src/pages/SobreNos/SobreNos.js
+++ ouvidoria/src/pages/SobreNos/SobreNos.js
@@ -1,64 +1,51 @@
-import owlNav from '../../images/owl-nav.png';
-import iconOwl from '../../images/icon-owl.png';
-import sobreNosWoman from '../../images/sobre-nos-woman.png';
-import { Body, NavBarSobre, DivAzul, DivBranca, FooterSobre } from "./SobreNosStyle";
+import iconOwl from "../../images/icon-owl.png";
+import sobreNosWoman from "../../images/sobre-nos-woman.png";
+import { DivAzul, DivBranca } from "./SobreNosStyle";
+import MenuRegistro from "../../components/Menu/MenuRegistro";
+import Footer from "../../components/Footer/Footer";
 
-export default function SobreNos(){
-
-    return(
-
-        <>
-        <Body>
-            <NavBarSobre>
-            <header>
-                <img src={owlNav} alt="logotipo" id="logotipo" class="owl" />
-                <nav>
-                <ul>
-                    <li><a href="#">Início</a></li>
-                    <li><a href="#">Meu Perfil</a></li>
-                    <li><a href="#">Ajuda</a></li>
-                </ul>
-                </nav>
-            </header>
-            </NavBarSobre>
-            <DivAzul>
-            <section>
-                <div class="divAzul">
-                    <div class="divCoruja"> <img src={iconOwl} alt="icone-owl" id="icone-owl" /> </div>
-                    <div class="divTexto1">
-                        <p> Somos uma plataforma de ouvidoria desenvolvida 
-                            para facilitar o monitoramento dos desafios infraestruturais
-                            que possam comprometer o bem-estar da comunidade.</p>
-                    </div>
-                </div>
-            </section>
-            </DivAzul>
-            <DivBranca>
-            <section>
-                <div class="divBranca">
-                    <div class="divTexto2">
-                        <p> Com a sua informação, podemos monitorar esses problemas de forma rápida e agir de forma prática!</p>
-                        <button type="button" class="botao-time"> Conheça nosso time! </button>
-                    </div>
-                    <div class="divWoman"> <img src={sobreNosWoman} alt="sobre-nos-woman" id="woman" /> </div>
-                </div>
-            </section>
-            </DivBranca>
-            <FooterSobre>
-            <span>
-                <div id="divFooter">
-                <div id="rodape">
-                    <p><a href="#">SOBRE OS DESENVOLVEDORES</a></p>
-                    <p><a href="#">PERGUNTAS FREQUENTES</a></p>
-                    <p><a href="#">FALE COM A GENTE</a></p>
-                </div>      
-                <div id="copyright">
-                    <p>©REACTon 2022. Todos os Direitos Reservados</p>
-                </div>
-                </div>    
-            </span> 
-            </FooterSobre>
-        </Body>
-        </>
-    )
+export default function SobreNos() {
+  return (
+    <>
+      <MenuRegistro />
+        <DivAzul>
+          <section>
+            <div className="divAzul">
+              <div className="divCoruja">
+                <img src={iconOwl} alt="icone-owl" id="icone-owl" />{" "}
+              </div>
+              <div className="divTexto1">
+                <p>
+                  Somos uma plataforma de ouvidoria desenvolvida para facilitar
+                  o monitoramento dos desafios infraestruturais que possam
+                  comprometer o bem-estar da comunidade.
+                </p>
+              </div>
+            </div>
+          </section>
+        </DivAzul>
+        <DivBranca>
+          <section>
+            <div className="divBranca">
+              <div className="divTexto2">
+                <p>
+                  Com a sua informação, podemos monitorar esses problemas de forma rápida e agir de forma prática!
+                </p>
+                <button type="button" className="botao-time">
+                  Conheça nosso time!
+                </button>
+              </div>
+              <div className="divWoman">
+                <img
+                  src={sobreNosWoman}
+                  alt="sobre-nos-woman"
+                  id="woman"
+                />
+              </div>
+            </div>
+          </section>
+        </DivBranca>
+        <Footer/>
+    </>
+  );
 }
--- ouvidoria/src/pages/SobreNos/SobreNosStyle.js
+++ ouvidoria/src/pages/SobreNos/SobreNosStyle.js
@@ -1,67 +1,5 @@
 import styled from "styled-components";
 
-export const Body = styled.main`
-
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Nanum+Gothic&family=Source+Sans+Pro&display=swap");
-
-
-body {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  background-color: #f8f8ff;
-  font-family: "Montserrat", sans-serif;
-  color: #5a5757;
-}
-`;
-
-export const NavBarSobre = styled.section`
-
-header {
-    width: 100%;
-    height: 70px;
-    background-color: #235ae2;
-    box-shadow: 0 5px 6px 0 rgba(0, 0, 0, 0.35);
-    display: flex;
-    align-content: center;
-    justify-content: space-between;
-    align-items: center;
-    position: fixed;
-    top: 0;
-  }
-  .owl {
-    width: 10rem;
-  }
-  #logotipo {
-    margin-left: 14%;
-  }
-  nav {
-    justify-content: center;
-    align-items: center;
-    margin-right: 14%;
-  }
-  ul {
-    list-style-type: none;
-    display: flex;
-    align-items: center;
-    margin: 0;
-    padding: 0;
-  }
-  li a {
-    text-decoration: none;
-    list-style-type: none;
-    color: aliceblue;
-    font-size: 18px;
-    margin-right: 20px;
-    padding: 22px 4px;
-    word-spacing: 4px;
-  }
-  ul li a:hover {
-    border-bottom: 2px solid rgb(81, 230, 130);
-  }
-`;
-
 export const DivAzul = styled.div`
 
 .divAzul{
@@ -70,7 +8,7 @@ export const DivAzul = styled.div`
     background-color: #74ACE1;
     display: flex;
     justify-content:space-evenly;
-}
+} 
 .divCoruja{
     width: 200%;
     display: flex;
@@ -82,7 +20,7 @@ export const DivAzul = styled.div`
 }
 .divTexto1{
     text-align: justify;
-    font-size: 1.8rem;
+    font-size: 1.4rem;
     font-weight: 550;
     color: #282525;
     padding-right: 15%;
@@ -92,7 +30,7 @@ export const DivAzul = styled.div`
 export const DivBranca = styled.div`
 
 .divBranca{
-    padding: 5%;
+    padding: 3% 15%;
     background-color: #E5E5E5;
     display: flex;
     justify-content:space-around;
@@ -104,14 +42,14 @@ export const DivBranca = styled.div`
 .divTexto2 p {
     width: 80%;
     text-align: justify;
-    font-size: 1.8rem;
+    font-size: 1.4rem;
     font-weight: 550;
     color: #2539EA;
 }
 .botao-time{
     margin-top: 2.5rem;
     font-weight: 600;
-    font-size: 110%;
+    font-size: 1rem;
     text-align: center;
     width: 20rem; height: 3rem;
     border-radius: 0.5rem;
@@ -120,41 +58,42 @@ export const DivBranca = styled.div`
     color: white;
     cursor: pointer;
     box-shadow: 0 5px 6px 0 rgba(0, 0, 0, 0.35);
+    font-family: "Montserrat", sans-serif;
 }
 .divWoman img{
     width: 22rem;
 }
 `;
 
-export const FooterSobre = styled.section`
+// export const FooterSobre = styled.section`
 
-span{
-    background-color: #1348aa;
-    height: 8rem;
-    display: flex;
-    align-items: center;
-}
-#divFooter{
-    width: 100%;
-}
-#rodape{
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    text-align: center;
-    margin-left: 20%;
-    margin-right: 20%;
-}
-#copyright{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #ffff;
-    word-spacing: 3px;   
-}
-p > a{
-    text-decoration: none;
-    color: #ffff;
-    word-spacing: 4px;
-}
-`;
+// span{
+//     background-color: #1348aa;
+//     height: 8rem;
+//     display: flex;
+//     align-items: center;
+// }
+// #divFooter{
+//     width: 100%;
+// }
+// #rodape{
+//     display: flex;
+//     align-items: center;
+//     justify-content: space-around;
+//     text-align: center;
+//     margin-left: 20%;
+//     margin-right: 20%;
+// }
+// #copyright{
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     color: #ffff;
+//     word-spacing: 3px;   
+// }
+// p > a{
+//     text-decoration: none;
+//     color: #ffff;
+//     word-spacing: 4px;
+// }
+// `;
