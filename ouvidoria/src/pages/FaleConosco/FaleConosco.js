4ace5206537eb7423204147c6013ae90dc67704f
Arthur Aguiar <artthurgaguiar@hotmail.com>
2022-08-10 23:27:18 -0300

    Implementado envio de e-mail Tela Fale Conosco

--- ouvidoria/src/pages/FaleConosco/FaleConosco.js
+++ ouvidoria/src/pages/FaleConosco/FaleConosco.js
@@ -1,15 +1,37 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import contactUs from "../../images/contact-us.png"
 import { useNavigate } from "react-router-dom";
 import Menu from "../../components/Menu/MenuRegistro";
 import Footer from "../../components/Footer/Footer";
 import { Banner, Container, FormContact } from "./FaleConoscoStyle";
+import ModalEnviado from './ModalEnviado';
 
 export default function FaleConosco(){
 
+    const [open, setOpen] = useState(false);
+    
     const navigate = useNavigate();
+
+    const form = useRef();
+    const sendEmail = (e) => {
+    e.preventDefault();
+    if (!form.current || !form.current.nome.value || !form.current.email.value || !form.current.mensagem.value) {
+        setOpen(false)
+    } else {
+        emailjs.sendForm('service_minha_ouvidoria', 'template_bbfhk0i', form.current, 'B-ocLRogkYTc1nhZX')
+        .then((result) => {
+            console.log(result.text);
+            setOpen(true)
+        }).catch ((error) => {
+            console.log(error.text);
+            setOpen(false)
+        });
+    }};
 
     return(
         <>
+        <ModalEnviado open={open} setOpen={setOpen} />
         <Menu/>
         <Container>
             <Banner>
@@ -17,14 +39,13 @@ export default function FaleConosco(){
                 <img 
                   src={contactUs} alt="atendente" 
                 />
-                
             </Banner>
             <FormContact>
                 <div>
                 <span> 
                   <h3> Fale Conosco </h3>
                 </span>
-                <form>
+                <form ref={form} onSubmit={sendEmail}>
                     <div className="input-dados">
                         <input placeholder="Nome" type="text" name="nome" className="nomeCompleto" required/>
                     </div>
@@ -35,9 +56,9 @@ export default function FaleConosco(){
                         <textarea placeholder="Mensagem" cols="30" row="15" name="mensagem" required/>
                     </div>
                     <div className="enviar-dados">
-                        <input type="submit" defaultValue="Enviar" />
+                        <input type="submit" defaultValue="Enviar" variant="outlined" onClick={() => sendEmail()} />
                     </div>
-                </form>
+                </form>                
                 </div>
             </FormContact>
         </Container>
@@ -45,3 +66,10 @@ export default function FaleConosco(){
         </>
     )
 }
+
+// const handleClick = () => {
+//     console.log("funcionando " + setOpen);
+//     setOpen(true);
+// };
+    
+//const [exibirModal, setExibirModal] = useState(false)
---
+++ ouvidoria/src/pages/FaleConosco/ModalEnviado.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+export default function ModalEnviado (props) { 
+  
+  const handleClose = () => {
+    props.setOpen(false);
+  };
+
+  return (
+    <>
+      <Dialog
+        open={props.open} onClose={handleClose}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-describedby="alert-dialog-slide-description">
+        <DialogTitle>{"E-mail enviado com sucesso!"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Agradecemos seu contato, responderemos o mais breve possível :)
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}> Fechar </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   useEffect(() => {
+//     setOpen(props.open)
+//   }, []);
+   
+//  const handleClickOpen = () => {
+//     props.setOpen(true);
+//   };
